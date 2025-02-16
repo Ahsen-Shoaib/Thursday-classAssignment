51869cd82a9c983a7b1ede931d216e415d729407
Ahsen Shoaib <ahsenshoaib@gmail.com>
2025-02-17 00:10:31 +0500

    Assignment3

---
+++ Assignment 3/app/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero1 from "./components/Hero1/Hero1";
+import Hero2 from "./components/Hero2/Hero2";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <Hero1 />
+      <Hero2/>
+      <Footer />
+    </>
+  );
+}
