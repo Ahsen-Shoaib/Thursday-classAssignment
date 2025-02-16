7ecc7590840be6ea7975b8eb89c5ac7c20240de1
Ahsen Shoaib <ahsenshoaib@gmail.com>
2025-02-17 00:16:14 +0500

    Assignment 6

---
+++ Assignment 6/app/page.tsx
@@ -0,0 +1,35 @@
+import Acheivements from "./components/Acheivements/Acheivements";
+import Courses from "./components/Courses/Courses";
+import CourseTabs from "./components/CourseTabs/CourseTabs";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
+import SponsorSection from "./components/SponsorSection/SponsorSection";
+import Team from "./components/Team/Team";
+import TestimonialCard from "./components/TestimonialSection/TestimonialSection";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+    
+
+
+    
+      
+      
+    
+  
+
+    <Header/>
+    <HeroSection/>
+    <SponsorSection/> 
+    <Courses/>
+    <Acheivements/>
+    <CourseTabs/>
+    <Team/>
+    <TestimonialCard/>
+    <Footer/>
+     </>
+  );
+}
